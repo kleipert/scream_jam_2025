543313650c74f972fbe2d98bae56811545657ece
Kevin <kleipert@web.de>
2025-10-12 14:15:00 +0200

    [WIP] event 11 wip

--- Assets/Scripts/Events/Event_11_GhostsAttack.cs
+++ Assets/Scripts/Events/Event_11_GhostsAttack.cs
@@ -1,12 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Enemies;
+using Player;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
 namespace Events
 {
     public class Event_11_GhostsAttack : EventBase
     {
+
+        [SerializeField] private List<GameObject> ghosts;
+        [SerializeField] private GameObject player;
+        private Event11PlayerData playerData;
+
         public override void StartEvent()
         {
             base.StartEvent();
             Event = Data.Events.GhostsAttack;
             Item = Data.EventsToItemsMap[Event];
+        }
+        
+        private void Start()
+        {
+            playerData = player.GetComponent<Event11PlayerData>();
+        }
+
+        private void Update()
+        {
+            var ghostToAttack = Random.Range(0, ghosts.Count);
+            
+            if(!playerData.getsAttacked)    
+                AttackPlayer(ghostToAttack);
+        }
+
+        private void AttackPlayer(int ghostIdx)
+        {
+            playerData.getsAttacked = true;
+            ghosts[ghostIdx].GetComponent<Event11Ghosts>().isAttacking = true;
         }
 
         public override void StopEvent()
---
+++ Assets/Scripts/Player/Event11PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Event11PlayerData : MonoBehaviour
+    {
+        public bool getsAttacked = false;
+        public uint ghostsKilled = 0;
+        public bool allGhostsKilled = false;
+
+        private void Update()
+        {
+            if (ghostsKilled == 5)
+                allGhostsKilled = true;
+        }
+    }
+}
