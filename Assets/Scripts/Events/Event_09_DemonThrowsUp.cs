17fd2f3f0b83151f464486042523799e2bed554c
Banbaro <leon_baur@hotmail.de>
2025-10-13 21:45:08 +0200

    [WIP] Event 09

--- Assets/Scripts/Events/Event_09_DemonThrowsUp.cs
+++ Assets/Scripts/Events/Event_09_DemonThrowsUp.cs
@@ -1,16 +1,111 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+using GameManager;
+
 namespace Events
 {
     public class Event_09_DemonThrowsUp : EventBase
     {
+        [SerializeField] private GameObject spine;
+        [SerializeField] private GameObject head;
+        [SerializeField] private GameObject laser;
+        
+        [SerializeField] float spineDegPerSec = 30f;
+        [SerializeField] float headDegPerSec = 150f;
+        
+        [SerializeField] float targetSpineZ = -100f;
+        
+        [SerializeField] float angleTolerance = 0.5f;
+
+        private bool _startEvent;
+        private bool _spineAtTarget;
+        private bool _returning;
+        
+        Quaternion baseSpineLocal, baseHeadLocal;
+        
         public override void StartEvent()
         {
             base.StartEvent();
             Event = Data.Events.DemonThrowsUp;
             Item = Data.EventsToItemsMap[Event];
+            
+            if (spine) baseSpineLocal = spine.transform.localRotation;
+            if (head)  baseHeadLocal  = head.transform.localRotation;
+            
+            _startEvent = true;
         }
+
+        void LateUpdate()
+        {
+            if (_startEvent)
+            {
+                if (spine != null && !_spineAtTarget)
+                {
+                    var e = spine.transform.localEulerAngles;
+                    e.z = Mathf.MoveTowardsAngle(e.z, targetSpineZ, spineDegPerSec * Time.deltaTime);
+                    float nextZ = Mathf.MoveTowardsAngle(e.z, targetSpineZ, spineDegPerSec * Time.deltaTime);
+                    e.z = nextZ;
+                    spine.transform.localEulerAngles = e;
+                    
+                    _spineAtTarget = Mathf.Abs(Mathf.DeltaAngle(nextZ, targetSpineZ)) <= angleTolerance;
+
+                    if (_spineAtTarget)
+                    {
+                        laser.gameObject.SetActive(true);
+                        StartCoroutine(EndEvent());
+                    }
+                }
+
+                if (head != null && _spineAtTarget)
+                {
+                    head.transform.Rotate(0f, headDegPerSec * Time.deltaTime, 0f, Space.Self);
+                }
+            }
+        }
+
+        IEnumerator EndEvent()
+        {
+            yield return new WaitForSeconds(10.0f);
+            EventController.instance.StopCurrentEvent();
+        } 
 
         public override void StopEvent()
         {
+            laser.gameObject.SetActive(false);
+            _returning = true;
+            _startEvent = false;
+            
+            StartCoroutine(ReturnToBaseThenFinish());
+        }
+        
+        IEnumerator ReturnToBaseThenFinish()
+        {
+            while (true)
+            {
+                bool spineDone = true, headDone = true;
+
+                if (spine != null)
+                {
+                    spine.transform.localRotation = Quaternion.RotateTowards(
+                        spine.transform.localRotation, baseSpineLocal, spineDegPerSec * Time.deltaTime);
+                    spineDone = Quaternion.Angle(spine.transform.localRotation, baseSpineLocal) <= angleTolerance;
+                }
+
+                if (head != null)
+                {
+                    head.transform.localRotation = Quaternion.RotateTowards(
+                        head.transform.localRotation, baseHeadLocal, headDegPerSec * Time.deltaTime);
+                    headDone = Quaternion.Angle(head.transform.localRotation, baseHeadLocal) <= angleTolerance;
+                }
+
+                if (spineDone && headDone) break;
+                yield return null;
+            }
+
+            _returning = false;
+            
             base.StopEvent();
         }
     }
